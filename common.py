0880e9e9a3510552f59d29561a38327e8ac8bc3c
mfschmidt <mikeschmidt@schmidtgracen.com>
2019-08-02 15:48:34 -0400

    moved some common constants to a common file

---
+++ common.py
@@ -0,0 +1,6 @@
+# The original source of all data is an excel workbook.
+source_file = "data.xlsx"
+
+# Those data are transformed into two data tables (dataframes)
+data_file = "data.tsv"
+meta_file = "meta.tsv"
