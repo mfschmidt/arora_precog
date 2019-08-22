4ffe97b76ba9ad1bca1693dea0f8735f433efb6c
mfschmidt <mikeschmidt@schmidtgracen.com>
2019-08-21 21:56:33 -0400

    moved some repeated constants to common.py

--- common.py
+++ common.py
@@ -4,3 +4,11 @@ source_file = "data.xlsx"
 # Those data are transformed into two data tables (dataframes)
 data_file = "data.tsv"
 meta_file = "meta.tsv"
+
+# Color schemes for plots
+import seaborn as sns
+red_to_blue = sns.color_palette("RdBu", 10)
+dark_palette = [red_to_blue[1], red_to_blue[8], ]
+light_palette = [red_to_blue[3], red_to_blue[6], ]
+reddish = red_to_blue[1]
+bluish = red_to_blue[8]
