05c49fe02aad073b663fe1719ed6fd5f2e5f5738
mfschmidt <mikeschmidt@schmidtgracen.com>
2022-05-17 11:13:36 -0400

    added grayscale options for printable figures

--- common.py
+++ common.py
@@ -1,3 +1,6 @@
+import seaborn as sns
+
+
 # The original source of all data is an excel workbook.
 source_file = "data.xlsx"
 
@@ -6,9 +9,13 @@ data_file = "data.tsv"
 meta_file = "meta.tsv"
 
 # Color schemes for plots
-import seaborn as sns
 red_to_blue = sns.color_palette("RdBu", 10)
 dark_palette = [red_to_blue[1], red_to_blue[8], ]
 light_palette = [red_to_blue[3], red_to_blue[6], ]
 reddish = red_to_blue[1]
 bluish = red_to_blue[8]
+
+grayscale = sns.color_palette("light:black", n_colors=20)
+dark_grays_palette = [grayscale[8], grayscale[14], ]
+light_grays_palette = [grayscale[2], grayscale[8], ]
+mid_grays_palette = [grayscale[4], grayscale[14], ]
